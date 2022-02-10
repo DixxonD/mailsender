857b71b85052c99aad3ac5696b089340ab933529
Dario Kaufmann <dario.kaufmann@students.bfh.ch>
2022-02-10 20:42:53 +0100

    removed unused code

--- src/main/kotlin/ch/tvlla/mailsender/security/BlacklistRepository.kt
+++
@@ -1,7 +0,0 @@
-package ch.tvlla.mailsender.security
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BlacklistRepository: JpaRepository<RemoteClient, String> {
-    fun deleteAllByLastSeenLessThan(before: Long)
-}
